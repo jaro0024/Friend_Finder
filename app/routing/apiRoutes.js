57ed3c4f6c4e1c1d10524984811b84027dc5e147
daniela jaros <danielajaros@gmail.com>
2018-02-26 14:59:26 -0500

    deleted a comment

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,8 +4,4 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friendsData);
     });
-
-    // app.post("/api/friends", function(req, res) {
-        
-    // })
 };
