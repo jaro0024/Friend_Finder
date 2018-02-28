782b5fa67d21290a050afff3401b974c13e5dbce
daniela jaros <danielajaros@gmail.com>
2018-02-28 16:12:59 -0500

    got it to work, now I need to fix minor stuff

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,13 +4,49 @@ var path = require("path");
 
 // Routing
 module.exports = function (app) {
-    // API GET request 
+    // API GET Request for json data of all users
     app.get("/api/friends", function (req, res) {
         res.json(friends);
     });
-    // API POST Request
+    // API POST Request of user data
     app.post("/api/friends", function (req, res) {
-      friends.push(req.body);
-      res.json(friends);
+        
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        // Take the result of the user's survey POST and parse it
+        var userData = req.body;
+        var userScores = userData.scores;
+        // To calculate the difference between the user's scores and the scores of each user in database
+        var totalDifference = 0;
+        // Loop through all users in database
+        for (var i = 0; i < friends.length; i++) {
+            totalDifference = 0;
+            // Loop through the scores of all users
+            for(var j = 0; j < friends[i].scores[j]; j++) {
+                // Calculate the difference between scores and add them into the totalDifference
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+                // Check if the sum is less than the differences of current best match
+                if(totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friends[i].name;
+                    bestMatch.photo = friends[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+        friends.push(userData);
+        res.json(bestMatch);
     });
-};
+}
+
+
+
+
+
+
+
+
+
+
